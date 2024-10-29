58c287ead6a2bd9ac6a202228d676b6d52a01fe9
e1iguerron789 <130955581+e1iguerron789@users.noreply.github.com>
2024-10-29 07:45:43 -0500

    Creacion Clase EGuerron e inicio de clase Celular en Modelos

---
+++ Guerron_Elizabeth-EXAMENPROGRESO/Models/Celular.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Guerron_Elizabeth_EXAMENPROGRESO.Models
+{
+    public class Celular
+    {
+        [Key]
+        public int Id
+    }
+}
---
+++ Guerron_Elizabeth-EXAMENPROGRESO/Models/EGuerron.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Guerron_Elizabeth_EXAMENPROGRESO.Models
+{
+    public class EGuerron
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public float Altura { get; set; }
+        [MaxLength(20)]       
+        public string Nombre { get; set; }
+
+        [EmailAddress]
+        public string Correo { get; set; }
+
+        public bool ClienteAntiguo { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Pedido { get; set; }
+
+
+
+    }
+}
