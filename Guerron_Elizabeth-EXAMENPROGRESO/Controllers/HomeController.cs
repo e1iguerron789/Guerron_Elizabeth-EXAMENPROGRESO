16570be60445c029fc5e9dbe7a205f0157ecb92c
e1iguerron789 <130955581+e1iguerron789@users.noreply.github.com>
2024-10-29 07:23:52 -0500

    Agregar archivos de proyecto.

---
+++ Guerron_Elizabeth-EXAMENPROGRESO/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Guerron_Elizabeth_EXAMENPROGRESO.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Guerron_Elizabeth_EXAMENPROGRESO.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
